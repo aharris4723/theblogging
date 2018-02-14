5649a4e4c0ba70a9fc3d128195c8e977405ada6b
unknown <minghaozhou123@gmail.com>
2018-02-14 17:06:39 -0500

    feed page and sign up page skeleton

--- app.rb
+++ app.rb
@@ -4,3 +4,22 @@ require 'sinatra/activerecord'
 set :database, 'sqlite3:main.sqlite3'
 set :sessions, true
 require './models'
+
+
+get '/' do
+	erb :login
+end
+
+get '/signup' do 
+	erb :"users/signup"
+end
+
+post '/create_account' do
+	User.create(username: params[:username], password: params[:password])
+	redirect '/login'
+end
+
+get '/feed' do
+	@blogs = Blog.all
+	erb :feed
+end
