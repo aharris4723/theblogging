829c5982a7706f40659b5fc7276cb9187262de15
John <jjh5166@gmail.com>
2018-02-14 17:37:00 -0500

    form creation

--- app.rb
+++ app.rb
@@ -5,7 +5,23 @@ set :database, 'sqlite3:main.sqlite3'
 set :sessions, true
 require './models'
 
-get '/' do 
-	@blogs = Blog.all
-	erb :login	
+get '/' do
+
+	erb :login
+end
+
+post '/signin' do
+	@username = params[:username]
+	@password = params[:password]
+	if
+		user = User.where(username: @username, password: @password).first
+		session[:user_id] = user.id
+		redirect '/feed'
+	else
+		redirect '/'
+	end
+end
+
+get '/users' do
+	erb :users
 end
