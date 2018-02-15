d18d72ef10ae1236abf5a5141203b46713b81d8b
John <jjh5166@gmail.com>
2018-02-15 16:21:33 -0500

    users functionality

--- app.rb
+++ app.rb
@@ -23,6 +23,7 @@ post '/signin' do
 end
 
 get '/users' do
+	@users = User.all
 	erb :users
 end
 
@@ -57,8 +58,15 @@ get '/profile' do
 	@blogs = Blog.all
 erb :profile
 end
+
 post "/create_blog" do
 user = User.find(session[:user_id])
 Blog.create(title: params[:title], content: params[:content], user_id: user.id)
 redirect '/'
+end
+
+get '/users/:id' do
+	user = User.find(params[:id])
+	@blogs = user.blogs
+	erb :"users/show"
 end
